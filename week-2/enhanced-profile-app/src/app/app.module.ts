0e93b02cdbf97af43f6a39592740361172ce89d5
Keith Hall <hkeith62@gmail.com>
2022-01-12 19:08:17 -0600

    Exercise 2.3- Data Bindin

---
+++ week-2/enhanced-profile-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+ /*
+============================================
+; Title:  App Module
+; Author: Professor Krasso
+; Date: 01/11/2022
+; Modified By: Keith Hall
+; Description: App Module of the enhance-profile-app
+;===========================================
+*/
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
