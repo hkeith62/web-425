abf3cb0959571c6550a0ca8823c5973224a04737
Keith Hall <hkeith62@gmail.com>
2022-02-08 21:42:11 -0600

    Initial submit of gpa-calculator-app

---
+++ week-6/gpa-calculator-app2/src/app/not-found/not-found.component.ts
@@ -0,0 +1,24 @@
+ /*
+============================================
+; Title: not-found.component.ts
+; Author: Professor Krasso
+; Date: 02/06/2022
+; Modified By: Keith Hall
+; Description: component.ts file for not-found component.
+;===========================================
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,13 @@
+ /*
+============================================
+; Title: transcript.interface.ts
+; Author: Professor Krasso
+; Date: 02/08/2022
+; Modified By: Keith Hall
+; Description: Transcript object.
+;===========================================
+*/
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
